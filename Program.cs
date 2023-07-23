3e8ab6308d1cca5439e4d50daba7277348c20498
Alexandre Machado <alexandre.machado@techdrivendigital.com>
2023-07-23 15:44:39 -0300

    desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Começando");
+
+Nokia nk = new Nokia("9999-9999","Nokia","seilaImei",64);
+nk.Ligar();
+nk.InstalarAplicativo("Novo appp");
+
+Console.WriteLine("\n");
+
+Iphone iphone = new Iphone("88888-8888", "Iphone", "seilaImei", 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Novo appp");
